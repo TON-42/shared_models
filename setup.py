9505bacc2999258c16be2d3280e970c6eb6d86be
552020 <stefanolombardo@posteo.de>
2024-06-09 00:25:37 +0200

    chore: setup shared models

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="shared_models",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "sqlalchemy",
+        "SQLAlchemy>=1.3.0",
+        "python-dotenv>=0.10.0",
     ],
 )
